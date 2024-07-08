c7b9e22548684e30d8323a2cc51c782775fa5cac
larazevedo <lara.azevedo@outlook.com>
2024-07-08 12:50:26 -0300

    Aula 03

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center">
+  <img alt="Logo NLW Journey - Rocketseat" src=".github/logo.png" width="200px" />
+</p>
+
+<p align="center">
+Aplicação desenvolvida no NLW Journey da Rocketseat na trilha HTML+CSS+JS.
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=18181B&labelColor=BEF264">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Preview do projeto desenvolvido." src=".github/preview.png" width="60%">
+</p>
+
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+## Projeto
+
+Nesse projeto iremos desenvolver uma versão simplificada de um sistema de roteiro de viagem!
--- script.js
+++ script.js
@@ -56,10 +56,27 @@ const criarItemDeAtividade = (atividade) => {
   const formatar = formatador(atividade.data);
 
   return `
-    <div>
+    <div class="card-bg">
       ${input}
+
+      <div>
+        <svg class="active" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.50008 10L9.16675 11.6667L12.5001 8.33335M18.3334 10C18.3334 14.6024 14.6025 18.3334 10.0001 18.3334C5.39771 18.3334 1.66675 14.6024 1.66675 10C1.66675 5.39765 5.39771 1.66669 10.0001 1.66669C14.6025 1.66669 18.3334 5.39765 18.3334 10Z" stroke="#BEF264" style="stroke:#BEF264;stroke:color(display-p3 0.7451 0.9490 0.3922);stroke-opacity:1;" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+
+      <svg class="inactive" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.41664 1.81836C9.46249 1.61597 10.5374 1.61597 11.5833 1.81836M11.5833 18.1817C10.5374 18.3841 9.46249 18.3841 8.41664 18.1817M14.6741 3.10086C15.5587 3.70022 16.3197 4.46409 16.9158 5.35086M1.8183 11.5834C1.6159 10.5375 1.6159 9.46255 1.8183 8.4167M16.8991 14.6742C16.2998 15.5588 15.5359 16.3198 14.6491 16.9159M18.1816 8.4167C18.384 9.46255 18.384 10.5375 18.1816 11.5834M3.1008 5.32586C3.70016 4.44131 4.46403 3.68026 5.3508 3.0842M5.3258 16.8992C4.44124 16.2998 3.6802 15.536 3.08414 14.6492" stroke="#A1A1AA" style="stroke:#A1A1AA;stroke:color(display-p3 0.6314 0.6314 0.6667);stroke-opacity:1;" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      
       <span>${atividade.nome}</span>
-      <time>${formatar.dia.semana.longo}, 
+      </div>
+
+      <time class="short">
+      ${formatar.dia.semana.curto}.
+      ${formatar.dia.numerico} <br>
+      ${formatar.hora}
+      </time>
+      <time class="full">${formatar.dia.semana.longo}, 
       dia ${formatar.dia.numerico}
       de ${formatar.mes} 
       às ${formatar.hora}h </time>
@@ -167,7 +184,7 @@ const concluirAtividade = (event) => {
     return atividade.data == dataDesteInput
   })
 
-  if(!atividade) {
+  if (!atividade) {
     return
   }
 
